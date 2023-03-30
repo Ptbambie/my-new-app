dd1a5e87a1c7576e8f0f23146852959a41f31625
Audrey Alaime <audreyalaime3@gmail.com>
2023-03-30 07:40:36 +0200

    props.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,20 @@ import PokemonCard from "./component/PokemonCard"
 
 function App() {
  return (
-      <div>
-        <PokemonCard />
+      <div className='pokeball'>
+        <PokemonCard pokemon = {pokemonList[0]}/>
       </div>   
   );
 }
+const pokemonList = [
+  {
 
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 export default App
